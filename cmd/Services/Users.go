ab2b8f03b64b9b70561d9efaf37e001e40eb66e2
selemene112 <ariyanda98y@gmail.com>
2023-11-15 19:57:41 +0800

    Add Service for get all users'

--- cmd/Services/Users.go
+++ cmd/Services/Users.go
@@ -1,1 +1,27 @@
 package services
+
+import (
+	"context"
+	UsersPb "gprc_protocolbuffer/pkg"
+)
+
+type UserServices struct {
+	UsersPb.UnimplementedProductServiceServer
+}
+
+// func GetAllUsers() *UsersPb.Users {
+// 	return &UsersPb.Users{}
+// }
+
+func (p *UserServices) GetAll(context.Context, *UsersPb.Empty) (*UsersPb.Users, error) {
+	Users := &UsersPb.Users{
+		Id:          19,
+		Username:    "test",
+		Password:    "test",
+		Email:       "test",
+		Name:        "testing",
+		PhotoProfil: "test",
+	}
+
+	return Users, nil
+}
