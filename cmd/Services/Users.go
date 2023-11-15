f443b090a4d7b0f268929c2bdb1a725cac366717
selemene112 <ariyanda98y@gmail.com>
2023-11-16 07:05:10 +0800

    Modified V1.0.0 Modif API for get all Data User

--- cmd/Services/Users.go
+++ cmd/Services/Users.go
@@ -2,26 +2,40 @@ package services
 
 import (
 	"context"
+	"fmt"
 	UsersPb "gprc_protocolbuffer/pkg"
+
+	"gorm.io/gorm"
 )
 
 type UserServices struct {
 	UsersPb.UnimplementedProductServiceServer
+	DB *gorm.DB
 }
 
 // func GetAllUsers() *UsersPb.Users {
 // 	return &UsersPb.Users{}
 // }
 
-func (p *UserServices) GetAll(context.Context, *UsersPb.Empty) (*UsersPb.Users, error) {
-	Users := &UsersPb.Users{
-		Id:          19,
-		Username:    "test",
-		Password:    "test",
-		Email:       "test",
-		Name:        "testing",
-		PhotoProfil: "test",
+// func (p *UserServices) GetAll(context.Context, *UsersPb.Empty) (*UsersPb.Users, error) {
+// 	GetUser := []*UsersPb.Users{}
+// 	p.DB.Find(&GetUser)
+// 	return &UsersPb.Users{}, nil
+// }
+
+func (p *UserServices) GetAll(ctx context.Context, req *UsersPb.Empty) (*UsersPb.UsersList, error) {
+	GetAllUser := []*UsersPb.Users{}
+
+	if err := p.DB.Find(&GetAllUser).Error; err != nil {
+		return nil, err
 	}
 
-	return Users, nil
+	fmt.Println(GetAllUser)
+
+	// Anda dapat menyalin langsung data GetAllUser ke UsersList
+	result := &UsersPb.UsersList{
+		Data: GetAllUser,
+	}
+
+	return result, nil
 }
